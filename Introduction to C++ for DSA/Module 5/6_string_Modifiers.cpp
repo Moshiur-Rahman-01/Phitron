9bb62e56a6105dd1b4428684f3e1b883e959a167
Md. Moshiur Rahman <moshiur.ict24@gmail.com>
2026-01-17 20:41:01 +0600

    added

--- Introduction to C++ for DSA/Module 5/6_string_Modifiers.cpp
+++ Introduction to C++ for DSA/Module 5/6_string_Modifiers.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    
+    string s1="Hello world";
+    string s2="World";
+    // s1+=s2;           use korbo
+    // s1.append(s2);
+    // s1.push_back('a');    last e 'a' hobe
+    // s1 += 'B';
+    // s2.pop_back();
+    // s1 = s2;
+    // s1.assign("Gello");
+    // s1.erase(2);
+    // s2.erase(3,2);    // 3 number index theke 2 ta character delete korbe
+    // s1.replace(6,5,"Bangladesh");
+    s1.insert(5,"Moshiur");
+    cout << s1 << endl;
+    cout << s2 << endl;
     return 0;
 }
